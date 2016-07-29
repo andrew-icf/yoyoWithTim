374cc09e679c08b590eccd1264e8dfe1c55cc7b5
Tim Musgrove <tim.musgrove91@gmail.com>
2016-07-29 16:01:41 -0600

    adding db connection

--- js/controllers.js
+++ js/controllers.js
@@ -2,9 +2,12 @@ angular
     .module("yoyoApp")
     .controller("MainController",  MainController)
 
-    function MainController($scope){
+    function MainController($scope, $http){
         $scope.view = {};
-        $scope.view.yoyos = yoyoData;
+        $http.get('https://cors-anywhere.herokuapp.com/https://afternoon-atoll-18846.herokuapp.com/yoyo').then(function(data) {
+            console.log(data.data)
+             $scope.view.yoyos = data.data;
+        })
         $scope.toggleVideo = function() {
             this.yoyo.videoToggle = !this.yoyo.videoToggle;
         }
